94e90123d6612ac626bd502186e9f9e6c5e16a13
Emmanuel Silva <emmanuelarankar@gmail.com>
2025-09-25 16:44:12 -0300

    v 0.0.3

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,27 +1,17 @@
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { Toaster } from 'react-hot-toast';
-import { Outlet, useLoaderData, useLocation } from 'react-router-dom';
-import { User } from '../../models';
-
-// Interface para garantir a tipagem dos dados do loader
-interface LayoutData {
-  profile: User | null;
-  initialFriendRequests: number;
-}
+import { Outlet, useLocation } from 'react-router-dom';
 
 export const Layout = () => {
-  // Esta linha agora funcionará, pois o loader está conectado
-  const { profile, initialFriendRequests } = useLoaderData() as LayoutData;
   const location = useLocation();
-  
   const noFooterRoutes = ['/login', '/register', '/forgot-password'];
   const shouldShowFooter = !noFooterRoutes.includes(location.pathname);
 
   return (
     <div className="min-h-screen bg-gray-50 w-full overflow-x-hidden">
-      {/* Passa os dados pré-carregados para o Header */}
-      <Header profile={profile} initialFriendRequests={initialFriendRequests} />
+      {/* # atualizado: Header não recebe mais props, eliminando a necessidade do useLoaderData */}
+      <Header />
       <main className="flex-1 w-full pt-20">
         <Outlet />
       </main>
