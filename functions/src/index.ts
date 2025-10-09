0c61de9f6973360ec921d5edafd2b36d9f404e18
Emmanuel Fernandes Silva <emmanuelarankar@gmail.com>
2025-10-09 18:14:33 -0300

    0.0.2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,14 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import cors from "cors";
 
+// Conecta o Admin SDK aos emuladores se estiver em ambiente local
+if (process.env.FUNCTIONS_EMULATOR) {
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099";
+  process.env.FIRESTORE_EMULATOR_HOST = "127.0.0.1:8080";
+  process.env.FIREBASE_STORAGE_EMULATOR_HOST = "127.0.0.1:9199";
+  console.log("✅ Conectado aos emuladores.");
+}
+
 // Inicializa o Firebase Admin SDK
 admin.initializeApp();
 
